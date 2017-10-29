f9d5398009d119e5b316b38a1815fe733a519407
RonanFelipe <feliperonan000@hotmail.com>
2017-10-28 22:44:54 -0200

    AVL Tree Implementation; Method to balance completed; Method rotation right and left completed; Main created; Movies objects created; Node objects created;

---
+++ src/AppTreeOfMovies/AppTree.java
@@ -0,0 +1,46 @@
+package AppTreeOfMovies;
+import TADMovie.*;
+import Tree.*;
+
+public class AppTree {
+    public static void main(String[] args){
+        Movies movie1 = new Movies("Public Enemies", 2009, 7.0, 140);
+        Movies movie2 = new Movies("American Gangster", 2007, 7.8, 157);
+        Movies movie3 = new Movies("Pirates of the Caribbean: Dead Men Tell No Tales", 2017, 6.8, 129);
+        Movies movie4 = new Movies("The Dark Knight", 2008, 9.0, 152);
+        Movies movie5 = new Movies("The Matrix", 1999, 8.7, 136);
+        Movies movie6 = new Movies("Inside Man", 2006, 7.6, 129);
+        Movies movie7 = new Movies("The GodFather", 1972, 9.2, 175);
+        Movies movie8 = new Movies("Casino Royale", 2006, 8.0, 144);
+        Movies movie9 = new Movies("The Last Samurai", 2003, 7.7, 154);
+        Movies movie10 = new Movies("Salt", 2010, 6.4, 100);
+        Movies movie11 = new Movies("The Lord of the Rings: The Fellowship of the Ring", 2001, 8.8, 178);
+        Movies movie12 = new Movies("The Lion King", 1994, 8.5, 98);
+        Movies movie13 = new Movies("Shutter Island", 2010, 8.1, 138);
+        Movies movie14 = new Movies("Mr. & Mrs. Smith", 2005, 6.5, 120);
+        Movies movie15 = new Movies("The Matrix Reloaded", 2003, 7.2, 138);
+        Movies movie16 = new Movies("Black Swan", 2010, 8.0, 108);
+        Movies movie17 = new Movies("Ocean's Eleven", 2001, 7.8, 116);
+        Movies movie18 = new Movies("V for Vendetta", 2005, 8.2, 132);
+        Node m1 = new Node(movie1, 7, null);
+        Node m2 = new Node(movie2, 8, null);
+        Node m3 = new Node(movie3, 6, null);
+        Node m4 = new Node(movie4, 20, null);
+        Node m5 = new Node(movie5, 17, null);
+        Node m6 = new Node(movie6, 5, null);
+        Node m7 = new Node(movie7, 21, null);
+        Node m8 = new Node(movie8, 15, null);
+        Node m9 = new Node(movie9, 9, null);
+        Node m10 = new Node(movie10, 5, null);
+        Node m11 = new Node(movie11, 18, null);
+        Node m12 = new Node(movie12, 14, null);
+        Node m13 = new Node(movie13, 13, null);
+        Node m14 = new Node(movie14, 4, null);
+        Node m15 = new Node(movie15, 10, null);
+        Node m16 = new Node(movie16, 12, null);
+        Node m17 = new Node(movie17, 11, null);
+        Node m18 = new Node(movie18, 16, null);
+        
+
+    }
+}
